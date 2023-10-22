22d7e78873f9692629415f4e143043d06b4483e1
Sebastian <iwan.sebastian029@gmail.com>
2023-10-22 18:23:06 +0200

    Naprawione

--- PrimitiveRenderer.cpp
+++ PrimitiveRenderer.cpp
@@ -69,34 +69,58 @@ void PrimitiveRenderer::drawLineIncremental(const sf::Vector2f& point1, const sf
 void PrimitiveRenderer::drawLineInstrukcja(const sf::Vector2f& startingPoint, const sf::Vector2f& endPoint, const sf::Color& color) {
     float delta_x = std::abs(endPoint.x - startingPoint.x);
     float delta_y = std::abs(endPoint.y - startingPoint.y);
-
-    float m = delta_y / delta_x;
-
     sf::Vector2f currentPoint = startingPoint;
+    float m = 0;
 
+    if (delta_x > delta_y) {
+        m = delta_y / delta_x;
+        for (int i = 0; i <= delta_x; i++) {
+            float temp = currentPoint.y;
+            currentPoint.y = (int)currentPoint.y;
+            sf::Vector2f step = sf::Vector2f(1, 1);
+            sf::RectangleShape pixel(step);
+            pixel.setPosition(currentPoint);
+            pixel.setFillColor(color);
+            window.draw(pixel);
+            currentPoint.y = temp;
 
-    for(int i=0;i<=delta_x;i++){
-        float temp = currentPoint.y;
-        currentPoint.y = (int)currentPoint.y;
-        sf::Vector2f step = sf::Vector2f(1, 1);
-        sf::RectangleShape pixel(step);
-        pixel.setPosition(currentPoint);
-        pixel.setFillColor(color);
-        window.draw(pixel);
-        currentPoint.y = temp;
-        
-        if(startingPoint.x < endPoint.x)
-            currentPoint.x += 1;
-        else
-            currentPoint.x -= 1;
+            if (startingPoint.x < endPoint.x)
+                currentPoint.x += 1;
+            else
+                currentPoint.x -= 1;
 
-        if (startingPoint.y < endPoint.y)
-            currentPoint.y += m;
-        else
-            currentPoint.y -= m;
-
-    
-    
-        
+            if (startingPoint.y < endPoint.y)
+                currentPoint.y += m;
+            else
+                currentPoint.y -= m;
+        }
     }
+    else {
+        m = delta_x / delta_y;
+        for (int i = 0; i <= delta_y; i++) {
+            float temp = currentPoint.x;
+            currentPoint.x = (int)currentPoint.x;
+            sf::Vector2f step = sf::Vector2f(1, 1);
+            sf::RectangleShape pixel(step);
+            pixel.setPosition(currentPoint);
+            pixel.setFillColor(color);
+            window.draw(pixel);
+            currentPoint.x = temp;
+
+            if (startingPoint.x < endPoint.x)
+                currentPoint.x += m;
+            else
+                currentPoint.x -= m;
+
+            if (startingPoint.y < endPoint.y)
+                currentPoint.y += 1;
+            else
+                currentPoint.y -= 1;
+        }
+    }
+
+    
+
+
+    
 }
