d367374d20afd1a5307cd815fc35cf439fa6bffd
Blazej <jakubczyk.blazej@gmail.com>
2023-11-26 00:41:50 +0100

    animate=0 żeby nie krzyczał

--- AnimatedObject.h
+++ AnimatedObject.h
@@ -8,6 +8,6 @@ public:
 	/// Wirtualna metoda odpowiedzialna za animowanie obiektu.
 	///
 	/// Metoda animate() jest wirtualn¹ funkcj¹, która powinna zostaæ zaimplementowana przez klasy dziedzicz¹ce po AnimatedObject.
-	virtual void animate();
+	virtual void animate()=0;
 };
 
